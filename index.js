658332e6dd4d780f33fd66d6f765c21d368175a4
Erick Rodríguez <e13antonio@gmail.com>
2022-06-02 16:39:10 -0600

    CROS trying to build it

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const app = express();
 require('dotenv').config();
 
 const port = process.env.PORT || 3005;
+const originVar = process.env.ORIGIN
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.afysi.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
@@ -18,6 +19,13 @@ mongoose.connect(uri,{useNewUrlParser: true}).then(()=>{
 })
 
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", originVar); 
+  // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api', api)
 
 app.listen(port, ()=>{
